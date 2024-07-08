2417f7e8d1eee9deb5befc5a85634030463e6a98
SwenBreitung <veroxa@web.de>
2024-07-08 04:09:03 +0000

    new server push

--- flix_tube_backend/settings.py
+++ flix_tube_backend/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
